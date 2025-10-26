d0901a8f3c41d4b49aa5c83a1c80771801aacee1
Remi <remi.lesanne@epfedu.fr>
2025-10-26 18:35:11 +0100

    creation vecteur dans qdrant

---
+++ ingest_qdrant.py
@@ -0,0 +1,39 @@
+from qdrant_client import QdrantClient
+from qdrant_client.models import VectorParams, Distance
+import pickle
+
+# Connexion à Qdrant local
+client = QdrantClient(host="localhost", port=6333) #via docker 
+
+client.recreate_collection( #attention bientot obsolète la méthode
+    collection_name="recettes",
+    vectors_config=VectorParams(
+        size=384,
+        distance=Distance.COSINE
+    )
+)
+print("Collection 'recettes' créée ✅")
+
+with open("documents.pkl", "rb") as f:
+    docs = pickle.load(f)
+
+with open("vectors.pkl", "rb") as f:
+    vectors = pickle.load(f)
+
+print(docs[0].meta)  # doit afficher {"title": "Spaghetti bolognaise"} par ex.
+
+points = [
+    {
+        "id": i,
+        "vector": v,
+        "payload": {"title": doc.meta["title"]}
+    }
+    for i, (v, doc) in enumerate(zip(vectors, docs))
+]
+
+
+client.upsert(
+    collection_name="recettes",
+    points=points
+)
+
